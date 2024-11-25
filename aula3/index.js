7751aeceef2290a9d37c574ccc1155858429da00
iBerserker89 <lucianof.barros89@gmail.com>
2024-11-25 19:15:02 -0300

    aula 1

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+console.log('Luciano "Barros"'); // string = texto
+console.log("Luciano 'Barros'"); // Number = número 
+console.log(`"Luciano" 'Barros'`);
+
+console.log(5 + 7, 5646.85); // é tudo Number, js não diferencia int de float.
+
+nome = 'Luciano';
+hora = 10;
+
+console.log(`Meu nome é`, nome, `. Estou aprendendo js às`, hora, ' da manhã.');
